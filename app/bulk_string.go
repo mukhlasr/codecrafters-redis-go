b6ae541f96e80ca654dba8d71045c73961d9b72f
Mukhlas Rosyadi <mukhlasrosyadi@outlook.com>
2024-03-04 16:27:21 +0700

    fix

--- app/bulk_string.go
+++ app/bulk_string.go
@@ -11,6 +11,10 @@ func EncodeBulkString(s string) string {
 }
 
 func EncodeBulkStrings(ss []string) string {
+	if len(ss) == 1 {
+		return EncodeBulkString(ss[0])
+	}
+
 	var sb strings.Builder
 	sb.WriteString(fmt.Sprintf("*%d\r\n", len(ss)))
 	for _, s := range ss {
